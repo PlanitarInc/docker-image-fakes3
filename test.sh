b8a6d204488cbc9c6b0ad25f2a5bafaee01deeac
korya <korya.github@gmail.com>
2020-01-30 09:11:21 -0500

    Make test.sh a bit more robust

--- test.sh
+++ test.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+cleanup() {
+  docker rm -f test-fakes3 test-bin || true
+}
+report() {
+  echo ""
+  echo "##### FAKES3 LOGS #####"
+  docker logs test-fakes3
+  echo ""
+  echo "##### TEST LOGS #####"
+  docker logs test-bin
+}
+trap report ERR
+trap cleanup EXIT
+
+
 docker run -d --name test-fakes3 -p :4567 ${IMAGE_NAME}
 
 sleep 3s
@@ -13,13 +28,11 @@ export PORT=$(docker inspect \
   -f '{{ (index (index .NetworkSettings.Ports "4567/tcp") 0).HostPort }}' \
   test-fakes3)
 
-docker run --rm -ti -v `pwd`/bin:/in --net host \
-  -e HOST=${HOST} -e PORT=${PORT} planitar/base /in/test
-res=$?; \
-if [ ${res} -ne 0 ]; then
-  docker logs test-fakes3
-  docker rm -f test-fakes3
-  exit 1
-fi
-
-docker rm -f test-fakes3
+docker run \
+  --name test-bin \
+  -v `pwd`/bin:/in \
+  --net host \
+  -e HOST=${HOST} \
+  -e PORT=${PORT} \
+  planitar/base \
+    /in/test

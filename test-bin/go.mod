0f1680c4cdfb1008ce20c9b9ce2baa0e87f98f09
korya <korya.github@gmail.com>
2020-05-09 22:33:02 -0400

    Use AWS SDK in tests

--- test-bin/go.mod
+++ test-bin/go.mod
@@ -3,7 +3,7 @@ module test-bin
 go 1.13
 
 require (
-	github.com/AdRoll/goamz v0.0.0-20170825154802-2731d20f46f4
+	github.com/aws/aws-sdk-go v1.30.24
 	github.com/google/go-cmp v0.4.0
 	github.com/kr/pretty v0.2.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- test-bin/main.go
+++ test-bin/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"sort"
 
-	"github.com/AdRoll/goamz/aws"
-	"github.com/AdRoll/goamz/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
 	fakes3host = getenv("HOST", "172.17.0.1")
 	fakes3port = getenv("PORT", "4567")
-	bucketname = "testbucket"
+	bucket     = "testbucket-plntr"
 )
 
 func getenv(key, defval string) string {
@@ -25,138 +31,252 @@ func getenv(key, defval string) string {
 }
 
 func main() {
-	auth := aws.Auth{
-		AccessKey: "abc",
-		SecretKey: "123",
-	}
-	fakeRegion := aws.Region{
-		Name:       "fakes3",
-		S3Endpoint: fmt.Sprintf("http://%s:%s", fakes3host, fakes3port),
-	}
-	s := s3.New(auth, fakeRegion)
-	bucket := s.Bucket(bucketname)
-	err := bucket.PutBucket(s3.BucketOwnerFull)
+	svc := s3.New(session.Must(session.NewSession(&aws.Config{
+		Region:           aws.String("fakes3"),
+		Endpoint:         aws.String(fmt.Sprintf("http://%s:%s", fakes3host, fakes3port)),
+		DisableSSL:       aws.Bool(true),
+		S3ForcePathStyle: aws.Bool(true),
+		Credentials:      credentials.NewStaticCredentials("key", "secret", ""),
+	})))
+
+	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+		ACL:    aws.String("bucket-owner-full-control"),
+	})
 	if err != nil {
-		panic(err.Error())
+		aerr, ok := err.(awserr.Error)
+		if !ok || aerr.Code() != s3.ErrCodeBucketAlreadyOwnedByYou {
+			panic(err.Error())
+		}
 	}
 
-	_, err = bucket.List("", "/", "", 20)
-	if err != nil {
-		panic(err.Error())
-	}
+	_, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Delimiter: aws.String("/"),
+	})
+	assertOK(err)
 
-	if err := testPutGet(bucket); err != nil {
-		panic(err.Error())
-	}
-
-	if err := testCopyObject(bucket); err != nil {
-		panic(err.Error())
-	}
-
-	if err := testCopyObject404(bucket); err != nil {
-		panic(err.Error())
-	}
-
-	if err := testDeleteObjects(bucket); err != nil {
-		panic(err.Error())
-	}
-
-	if err := testListEmptyDelimiter(bucket); err != nil {
-		panic(err.Error())
-	}
+	testPutGetDelete(svc)
+	testCopyObject(svc)
+	testCopyObject404(svc)
+	testListObjects(svc)
+	testDeleteObjects(svc)
 }
 
-func testPutGet(b *s3.Bucket) error {
-	key := "file.txt"
-	content := []byte("content")
+func testPutGetDelete(svc *s3.S3) {
+	const (
+		key         = "file.txt"
+		content     = "content"
+		contentType = "text/plain"
+	)
 
-	err := b.Put(key, content, "text/plain", s3.BucketOwnerFull, s3.Options{})
-	if err != nil {
-		return fmt.Errorf("cannot write file: %w", err)
-	}
+	put, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(key),
+		ContentType: aws.String(contentType),
+		ACL:         aws.String("bucket-owner-full-control"),
+		Body:        bytes.NewReader([]byte(content)),
+	})
+	assertOK(err)
+	assertAwsStringNotEmpty("ETag", put.ETag)
 
-	bs, err := b.Get(key)
-	if err != nil {
-		return fmt.Errorf("cannot read file: %w", err)
-	}
+	obj, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	assertOK(err)
+	defer obj.Body.Close()
+	assertAwsString("ETag", obj.ETag, *put.ETag)
+	assertAwsString("Content-Type", obj.ContentType, contentType)
+	assertReader("content", obj.Body, content)
 
-	if !bytes.Equal(bs, content) {
-		return fmt.Errorf("content doesn't match:\n- expected: '%s'\n- got: '%s'",
-			content, bs)
-	}
+	_, err = svc.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	assertOK(err)
 
-	return nil
-}
-
-func testCopyObject(b *s3.Bucket) error {
-	srcKey := "copy/src.txt"
-	srcContent := []byte("source")
-
-	err := b.Put(srcKey, srcContent, "text/plain", s3.BucketOwnerFull, s3.Options{})
-	if err != nil {
-		return fmt.Errorf("cannot write src file: %w", err)
-	}
-
-	dstKey := "copy/dst.txt"
-
-	source := fmt.Sprintf("/%s/%s", b.Name, srcKey)
-	_, err = b.PutCopy(dstKey, s3.BucketOwnerFull, s3.CopyOptions{}, source)
-	if err != nil {
-		return fmt.Errorf("cannot copy file: %w", err)
-	}
-
-	bs, err := b.Get(dstKey)
-	if err != nil {
-		return fmt.Errorf("cannot read dst file: %w", err)
-	}
-
-	if !bytes.Equal(bs, srcContent) {
-		return fmt.Errorf("content doesn't match:\n- expected: '%s'\n- got: '%s'",
-			srcContent, bs)
-	}
-
-	return nil
-}
-
-func testCopyObject404(b *s3.Bucket) error {
-	dstKey := "copy-404/dst.txt"
-
-	source := fmt.Sprintf("/%s/copy-404/@@@unknown-file@@@", b.Name)
-	_, err := b.PutCopy(dstKey, s3.BucketOwnerFull, s3.CopyOptions{}, source)
+	_, err = svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
 	if err == nil {
-		return fmt.Errorf("expected copy to fail with 404, got <nil>")
+		panic(fmt.Sprintf("Object /%s/%s should not exist", bucket, key))
 	}
-
-	s3err, ok := err.(*s3.Error)
-	if !ok {
-		return fmt.Errorf("expected copy to fail with 404, got (%T) %w", err, err)
+	if aerr, ok := err.(awserr.Error); !ok || aerr.Code() != "NoSuchKey" {
+		panic(fmt.Sprintf("Object /%s/%s should not exist: %s", bucket, key, err))
 	}
-
-	if s3err.StatusCode != 404 || s3err.Code != "NoSuchKey" {
-		return fmt.Errorf("expected copy to fail with 404, "+
-			"got code=%s status=%d: %w", s3err.Code, s3err.StatusCode, err)
-	}
-
-	return nil
 }
 
-func testDeleteObjects(b *s3.Bucket) error {
+func testCopyObject(svc *s3.S3) {
+	const (
+		srcKey         = "copy/src.txt"
+		srcContent     = "source"
+		srcContentType = "text/plain"
+		dstKey         = "copy/dst.txt"
+	)
+
+	defer func() {
+		_, _ = svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(srcKey),
+		})
+		_, _ = svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(dstKey),
+		})
+	}()
+
+	put, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(srcKey),
+		ContentType: aws.String(srcContentType),
+		ACL:         aws.String("bucket-owner-full-control"),
+		Body:        bytes.NewReader([]byte(srcContent)),
+	})
+	assertOK(err)
+	assertAwsStringNotEmpty("ETag", put.ETag)
+
+	res, err := svc.CopyObject(&s3.CopyObjectInput{
+		Bucket:     aws.String(bucket),
+		Key:        aws.String(dstKey),
+		ACL:        aws.String("bucket-owner-full-control"),
+		CopySource: aws.String(fmt.Sprintf("/%s/%s", bucket, srcKey)),
+	})
+	assertOK(err)
+	assertAwsString("ETag", res.CopyObjectResult.ETag, *put.ETag)
+
+	obj, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(dstKey),
+	})
+	assertOK(err)
+	defer obj.Body.Close()
+	assertAwsString("ETag", obj.ETag, *put.ETag)
+	assertAwsString("Content-Type", obj.ContentType, srcContentType)
+	assertReader("content", obj.Body, srcContent)
+}
+
+func testCopyObject404(svc *s3.S3) {
+	const (
+		srcKey = "copy-404/@@@unknown-file@@@"
+		dstKey = "copy-404/dst.txt"
+	)
+
+	_, err := svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(srcKey),
+	})
+	if err == nil {
+		panic(fmt.Sprintf("Object /%s/%s should not exist", bucket, srcKey))
+	}
+	if aerr, ok := err.(awserr.Error); !ok || aerr.Code() != "NotFound" {
+		panic(fmt.Sprintf("Object /%s/%s should not exist: %s", bucket, srcKey, err))
+	}
+
+	_, err = svc.CopyObject(&s3.CopyObjectInput{
+		Bucket:     aws.String(bucket),
+		Key:        aws.String(dstKey),
+		ACL:        aws.String("bucket-owner-full-control"),
+		CopySource: aws.String(fmt.Sprintf("/%s/%s", bucket, srcKey)),
+	})
+	if err == nil {
+		panic(fmt.Sprintf("Copy of /%s/%s should fail", bucket, srcKey))
+	}
+	if aerr, ok := err.(awserr.Error); !ok || aerr.Code() != s3.ErrCodeNoSuchKey {
+		panic(fmt.Sprintf("Copy of /%s/%s should fail with NoSuchKey: %s",
+			bucket, srcKey, err))
+	}
+}
+
+func testListObjects(svc *s3.S3) {
 	var err error
 
-	mustPutTextFiles(b, []textFile{
+	mustPutTextFiles(svc, bucket, []textFile{
+		{"list/a.txt", "1"},
+		{"list/one/b.txt", "2"},
+		{"list/one/c.txt", "3"},
+		{"list/one/two/d.txt", "4"},
+		{"list/f/o/u/r/e.txt", "5"},
+		{"list/f/o/u/r/f.txt", "6"},
+		{"list/g.txt", "7"},
+	})
+	defer deleteAllKeys(svc, bucket, []string{
+		"list/a.txt",
+		"list/one/b.txt",
+		"list/one/c.txt",
+		"list/one/two/d.txt",
+		"list/f/o/u/r/e.txt",
+		"list/f/o/u/r/f.txt",
+		"list/g.txt",
+	})
+
+	res, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	assertOK(err)
+	assertListReponse(res, []string{
+		"list/a.txt",
+		"list/f/o/u/r/e.txt",
+		"list/f/o/u/r/f.txt",
+		"list/g.txt",
+		"list/one/b.txt",
+		"list/one/c.txt",
+		"list/one/two/d.txt",
+	}, nil)
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Delimiter: aws.String("/"),
+	})
+	assertOK(err)
+	assertListReponse(res, nil, []string{"list/"})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Prefix:    aws.String("list/"),
+		Delimiter: aws.String("/"),
+	})
+	assertOK(err)
+	assertListReponse(res, []string{"list/a.txt", "list/g.txt"},
+		[]string{"list/f/", "list/one/"})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Prefix:    aws.String("list"),
+		Delimiter: aws.String("/"),
+	})
+	assertOK(err)
+	assertListReponse(res, nil, []string{"list/"})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Prefix:    aws.String("list"),
+		Delimiter: aws.String("/"),
+		MaxKeys:   aws.Int64(2),
+	})
+	assertOK(err)
+	assertListReponse(res, nil, []string{"list/"})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String("unknown/prefix/"),
+	})
+	assertOK(err)
+	assertListReponse(res, nil, nil)
+}
+
+func testDeleteObjects(svc *s3.S3) {
+	var err error
+
+	mustPutTextFiles(svc, bucket, []textFile{
 		{"multi-del/a.txt", "1"},
 		{"multi-del/b.txt", "2"},
 		{"multi-del/c.txt", "3"},
 		{"multi-del/d.txt", "4"},
 		{"multi-del/e.txt", "5"},
 	})
-
-	res, err := b.List("multi-del/", "/", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeys(res, []string{
+	defer deleteAllKeys(svc, bucket, []string{
 		"multi-del/a.txt",
 		"multi-del/b.txt",
 		"multi-del/c.txt",
@@ -164,142 +284,203 @@ func testDeleteObjects(b *s3.Bucket) error {
 		"multi-del/e.txt",
 	})
 
-	// Try to delete multi-del/b.txt and some version of multi-del/e.txt.
-	// Only multi-del/b.txt should be gone, multi-del/e.txt is not deleted
-	// because deleting versions is not supported.
-	if err := b.DelMulti(s3.Delete{Objects: []s3.Object{
-		{Key: "multi-del/b.txt"},
-		{Key: "multi-del/e.txt", VersionId: "v1"},
-	}}); err != nil {
-		return fmt.Errorf("cannot delete files: %w", err)
-	}
-
-	res, err = b.List("multi-del/", "/", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeys(res, []string{
+	res, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	assertOK(err)
+	assertListReponse(res, []string{
 		"multi-del/a.txt",
+		"multi-del/b.txt",
+		"multi-del/c.txt",
+		"multi-del/d.txt",
+		"multi-del/e.txt",
+	}, nil)
+
+	// Try to delete multi-del/b.txt and non-existent multi-del/z.txt.
+	del, err := svc.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(bucket),
+		Delete: &s3.Delete{
+			Objects: []*s3.ObjectIdentifier{
+				{Key: aws.String("multi-del/b.txt")},
+				{Key: aws.String("multi-del/z.txt")},
+			},
+		},
+	})
+	assertOK(err)
+	assertDeleteRespKeys(del, []string{
+		"multi-del/b.txt",
+		"multi-del/z.txt",
+	})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	assertOK(err)
+	assertListReponse(res, []string{
+		"multi-del/a.txt",
+		"multi-del/c.txt",
+		"multi-del/d.txt",
+		"multi-del/e.txt",
+	}, nil)
+
+	// Try to delete multi-del/a.txt, multi-del/e.txt and already removed
+	// multi-del/b.txt.
+	// Success should be reported, multi-del/c.txt should be gone.
+	del, err = svc.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(bucket),
+		Delete: &s3.Delete{
+			Objects: []*s3.ObjectIdentifier{
+				{Key: aws.String("multi-del/a.txt")},
+				{Key: aws.String("multi-del/b.txt")},
+				{Key: aws.String("multi-del/e.txt")},
+			},
+		},
+	})
+	assertOK(err)
+	assertDeleteRespKeys(del, []string{
+		"multi-del/a.txt",
+		"multi-del/b.txt",
+		"multi-del/e.txt",
+	})
+
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	assertOK(err)
+	assertListReponse(res, []string{
+		"multi-del/c.txt",
+		"multi-del/d.txt",
+	}, nil)
+
+	// Remove the rest of the files. Success should be reported.
+	del, err = svc.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(bucket),
+		Delete: &s3.Delete{
+			Objects: []*s3.ObjectIdentifier{
+				{Key: aws.String("multi-del/a.txt")},
+				{Key: aws.String("multi-del/b.txt")},
+				{Key: aws.String("multi-del/c.txt")},
+				{Key: aws.String("multi-del/d.txt")},
+				{Key: aws.String("multi-del/e.txt")},
+			},
+		},
+	})
+	assertOK(err)
+	assertDeleteRespKeys(del, []string{
+		"multi-del/a.txt",
+		"multi-del/b.txt",
 		"multi-del/c.txt",
 		"multi-del/d.txt",
 		"multi-del/e.txt",
 	})
 
-	// Try to delete multi-del/a.txt, multi-del/e.txt and already removed
-	// multi-del/b.txt.
-	// Success should be reported, multi-del/c.txt should be gone.
-	if err := b.DelMulti(s3.Delete{Objects: []s3.Object{
-		{Key: "multi-del/a.txt"},
-		{Key: "multi-del/b.txt"},
-		{Key: "multi-del/e.txt"},
-	}}); err != nil {
-		return fmt.Errorf("cannot delete files: %w", err)
-	}
-
-	res, err = b.List("multi-del/", "/", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeys(res, []string{
-		"multi-del/c.txt",
-		"multi-del/d.txt",
+	res, err = svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
 	})
-
-	// Remove the rest of the files. Success should be reported.
-	if err := b.DelMulti(s3.Delete{Objects: []s3.Object{
-		{Key: "multi-del/a.txt"},
-		{Key: "multi-del/b.txt"},
-		{Key: "multi-del/c.txt"},
-		{Key: "multi-del/d.txt"},
-		{Key: "multi-del/e.txt"},
-	}}); err != nil {
-		return fmt.Errorf("cannot delete files: %w", err)
-	}
-
-	res, err = b.List("multi-del/", "/", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeys(res, nil)
-
-	return nil
-}
-
-func testListEmptyDelimiter(b *s3.Bucket) error {
-	var err error
-
-	mustPutTextFiles(b, []textFile{
-		{"empty-del/a.txt", "1"},
-		{"empty-del/one/b.txt", "2"},
-		{"empty-del/one/c.txt", "3"},
-		{"empty-del/one/two/d.txt", "4"},
-		{"empty-del/f/o/u/r/e.txt", "5"},
-		{"empty-del/f/o/u/r/f.txt", "6"},
-		{"empty-del/g.txt", "7"},
-	})
-
-	// Delimiter is set to '/', only elements
-	res, err := b.List("empty-del/", "/", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeysCommonPrefixes(res, []string{
-		"empty-del/a.txt",
-		"empty-del/g.txt",
-	}, []string{
-		"empty-del/f/",
-		"empty-del/one/",
-	})
-
-	res, err = b.List("empty-del/", "", "", 100)
-	if err != nil {
-		return fmt.Errorf("cannot list files: %w", err)
-	}
-
-	assertListRespKeysCommonPrefixes(res, []string{
-		"empty-del/a.txt",
-		"empty-del/f/o/u/r/e.txt",
-		"empty-del/f/o/u/r/f.txt",
-		"empty-del/g.txt",
-		"empty-del/one/b.txt",
-		"empty-del/one/c.txt",
-		"empty-del/one/two/d.txt",
-	}, nil)
-
-	return nil
+	assertOK(err)
+	assertListReponse(res, nil, nil)
 }
 
 type textFile struct {
 	Key, Content string
 }
 
-func mustPutTextFiles(b *s3.Bucket, files []textFile) {
+func mustPutTextFiles(svc *s3.S3, bucket string, files []textFile) {
 	for _, f := range files {
-		err := b.Put(f.Key, []byte(f.Content), "text/plain",
-			s3.BucketOwnerFull, s3.Options{})
+		_, err := svc.PutObject(&s3.PutObjectInput{
+			Bucket:      aws.String(bucket),
+			Key:         aws.String(f.Key),
+			ContentType: aws.String("text/plain"),
+			ACL:         aws.String("bucket-owner-full-control"),
+			Body:        bytes.NewReader([]byte(f.Content)),
+		})
+		assertOK(err)
+	}
+}
+
+func deleteAllKeys(svc *s3.S3, bucket string, keys []string) {
+	for _, k := range keys {
+		_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(k),
+		})
 		if err != nil {
-			panic(fmt.Sprintf("cannot write '%s': %s", f.Key, err))
+			fmt.Fprintf(os.Stderr, "Failed to delete '/%s/%s'\n", bucket, k)
 		}
 	}
 }
 
-func assertListRespKeysCommonPrefixes(res *s3.ListResp, expKeys, expPrefixes []string) {
-	assertListRespKeys(res, expKeys)
-	assertStringList(res.CommonPrefixes, expPrefixes,
-		"Unexpected common-prefixes in list result")
-}
-
-func assertListRespKeys(res *s3.ListResp, expKeys []string) {
+func assertListReponse(res *s3.ListObjectsV2Output, expKeys, expPrefixes []string) {
 	var keys []string
 	for _, c := range res.Contents {
-		keys = append(keys, c.Key)
+		keys = append(keys, *c.Key)
+	}
+	assertStringList(keys, expKeys, "unexpected keys in list result")
+
+	var prefixes []string
+	for _, p := range res.CommonPrefixes {
+		prefixes = append(prefixes, *p.Prefix)
+	}
+	assertStringList(prefixes, expPrefixes, "unexpected prefixes in list result")
+}
+
+func assertDeleteRespKeys(res *s3.DeleteObjectsOutput, expKeys []string) {
+	var keys []string
+	for _, c := range res.Deleted {
+		keys = append(keys, *c.Key)
 	}
 
-	assertStringList(keys, expKeys, "unexpected keys in list result")
+	// the order does not matter
+	sort.StringSlice(keys).Sort()
+	sort.StringSlice(expKeys).Sort()
+
+	assertStringList(keys, expKeys, "unexpected keys in delete result")
+}
+
+func assertOK(err error) {
+	if err != nil {
+		panic(fmt.Sprintf("Expected no error, got %q", err.Error()))
+	}
+}
+
+func assertAwsStringNotEmpty(name string, actual *string) {
+	if actual == nil {
+		panic(fmt.Sprintf("%s is missing", name))
+	}
+
+	if *actual == "" {
+		panic(fmt.Sprintf("%s is empty", name))
+	}
+}
+
+func assertAwsString(name string, actual *string, expected string) {
+	if actual == nil {
+		panic(fmt.Sprintf(`%s is empty:
+- expected: %q
+- actual:   <nil>
+`, name, expected))
+	}
+
+	if *actual != expected {
+		panic(fmt.Sprintf(`%s is wrong:
+- expected: %q
+- actual:   %q
+`, name, expected, *actual))
+	}
+}
+
+func assertReader(name string, r io.Reader, expected string) {
+	bs, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(fmt.Sprintf(`failed to read %s: %q`, name, err.Error()))
+	}
+
+	if !bytes.Equal(bs, []byte(expected)) {
+		panic(fmt.Sprintf(`%s doesn't match:
+- expected: %q
+- actual:   %q
+`, name, string(bs), expected))
+	}
 }
 
 func assertStringList(actual, expected []string, message string) {
